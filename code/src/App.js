fb6163f481707881ff0213a6228ccee018cef836
Anna Binnquist <Binnquist@annas-mbp-3.mynet>
2019-11-29 23:57:48 +0100

    first commit of react with map

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Album } from 'components/Album'
 import data from './data.json'
 
 console.log(data)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="mainContainer">
+      {data.albums.items.map((item) => (
+        <Album
+          key={item.id}
+          name={item.name}
+          item={item} />
+      ))}
     </div>
   )
 }
---
+++ code/src/components/Album.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './album.css'
+import logo from 'icons/play.svg'
+import logo2 from 'icons/heart.svg'
+import logo3 from 'icons/dots.svg'
+
+export const Album = (props) => (
+  <div className="album">
+    <h1><a href={props.item.external_urls.spotify}>{props.name}</a></h1>
+    <h3><a href={props.item.artists[0].external_urls.spotify}>{props.item.artists[0].name}</a></h3>
+    <div><img src={props.item.images[0].url} alt="" />
+    </div>
+    <div className="info">
+      <div className="hoverInfo"><img src={logo} alt="" /></div>
+      <div className="hoverInfo2"><img src={logo2} alt="" /></div>
+      <div className="hoverInfo3"><img src={logo3} alt="" /></div>
+    </div>
+  </div>
+)
