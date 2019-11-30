cacb2945affefba562f9cd75cfb77fd35a899239
Anna Binnquist <Binnquist@annas-mbp-3.mynet>
2019-11-30 19:19:53 +0100

    trying to fix the links but the .json is fucktarted

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -6,14 +6,15 @@ import logo3 from 'icons/dots.svg'
 
 export const Album = (props) => (
   <div className="album">
+    <div className="theText">
     <h1><a href={props.item.external_urls.spotify}>{props.name}</a></h1>
-    <h3>{props.item.artists[0].name}</h3>
+    <h3><a href={props.item.external_urls.spotify}>{props.item.artists[0].name}</a></h3></div>
     <div><img src={props.item.images[0].url} alt="" />
     </div>
     <div className="info">
     <div className="hoverInfo"><a href={props.item.external_urls.spotify}><img src={logo} alt="" /></a></div>
-     <div className="hoverInfo2"><img src={logo2} alt="" /></div>
-      <div className="hoverInfo3"><img src={logo3} alt="" /></div>
+    <div className="hoverInfo2"><img src={logo2} alt="" /></div>
+    <div className="hoverInfo3"><img src={logo3} alt="" /></div>
     </div>
   </div>
 )
